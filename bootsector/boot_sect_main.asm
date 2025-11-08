bd0664ca00d734fbecbfd23937f18ececa5f4cd9
Lucas <lucascastineiras100@gmail.com>
2025-11-07 22:27:02 -0600

    Creating base for main bootsector, added .bin to the gitignore, created basic print function for assembly, organized files

---
+++ bootsector/boot_sect_main.asm
@@ -0,0 +1,25 @@
+[org 0x7c00]       ;global offset
+
+    mov bx, HELLO 
+    call print
+    call print_nl
+
+    mov bx, GOODBYE
+    call print
+    call print_nl
+
+    jmp $   ;infinite loop 
+
+    %include "./bootsector-functions/boot_sect_print.asm"
+    %include "./bootsector-functions/boot_sect_print_hex.asm"
+
+    HELLO:
+        db 'Hello,World',0
+    
+    GOODBYE:
+        db 'Goodbye',0
+
+; Fill with 510 zeros minus the size of the previous code
+    times 510-($-$$) db 0
+    dw 0xaa55 ; Magic number
+
---
+++ bootsector/bootsector-functions/boot_sect_print_hex.asm
@@ -0,0 +1,34 @@
+print_hex:
+    pusha
+
+    mov cx,0
+
+hex_loop:
+    cmp cx,4 ; loop 4 times
+    je end
+    
+    ; covert last char od 'dx' to ASCII
+    mov ax,dx 
+    and ax,0x000f 
+    add al,0x30 ; add 0x30 to N to convert into ASCII N
+    cmp al,0x39 ; if > 9, add extra 8 to represent 'A' to 'F'
+    jle step2
+    add al,7 ;'A' is ASCII 65 instead of 58, so 65-68= 7
+
+step2:
+    mov bx,HEX_OUT + 5 ; base +length
+    sub bx, cx
+    mov [bx], al 
+    ror dx, 4
+    
+    add cx,1
+    jmp hex_loop
+
+end:
+    mov bx,HEX_OUT
+    call print
+    popa
+    ret
+
+HEX_OUT:
+    db '0x0000',0
