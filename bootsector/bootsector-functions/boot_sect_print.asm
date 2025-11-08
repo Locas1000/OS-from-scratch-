ed04a7249b7e9493c1f746685e40179318ae4ab1
Lucas <lucascastineiras100@gmail.com>
2025-11-07 22:27:02 -0600

    Creating base for main bootsector, added .bin to the gitignore, created basic print function for assembly, organized files

---
+++ bootsector/bootsector-functions/boot_sect_print.asm
@@ -0,0 +1,29 @@
+print:
+    pusha
+
+;while (string[i] !=0) {print string[i]; i++}
+start:
+    mov al, [bx] ;base address fot the string 
+    cmp al,0
+    je done ;jump if equal/zero
+
+    mov ah, 0x0e
+    int 0x10
+
+    add bx,1
+    jmp start
+
+done:
+    popa
+    ret
+
+print_nl:
+    pusha
+    mov ah,0x0e
+    mov al,0x0a ;newline 
+    int 0x10
+    mov al, 0x0d ;return
+    int 0x10
+    
+    popa 
+    ret
