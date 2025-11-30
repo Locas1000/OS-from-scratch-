888e37f52f491ab9011710e3a00aa68e54a5a1b5
Lucas <lucascastineiras100@gmail.com>
2025-11-30 15:07:18 -0600

    Fixed small errors in code

--- kernel/kernel.c
+++ kernel/kernel.c
@@ -1,15 +1,20 @@
-#include "../drivers/screen.h"
+#include "../drivers/ports.h" 
 
 void main() {
-   port_byte_out(0x3d4, 14);
+    /* 1. Ask VGA hardware for cursor position */
+    port_byte_out(0x3d4, 14);
     int position = port_byte_in(0x3d5);
     position = position << 8;
 
-     port_byte_out(0x3d4, 15);
+    port_byte_out(0x3d4, 15);
     position += port_byte_in(0x3d5);
 
+    /* 2. Write 'X' at the cursor position */
     int offset_from_vga = position * 2;
-     char *vga = 0xb8000;
+    
+    /* FIXED: Add (char*) cast to prevent warning */
+    char *vga = (char*) 0xb8000;
     vga[offset_from_vga] = 'X'; 
-    vga[offset_from_vga+1] = 0x0f;
+    /* FIXED: Use Red (0x4F) instead of White (0x0f) to make it visible */
+    vga[offset_from_vga+1] = 0x4F;
 }
